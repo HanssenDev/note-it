e0c33f4c9e711b606b3e4155ae4a0316c6f0c92c
Hanssen <hramanjooloo@gmail.com>
2020-08-12 19:49:45 +0100

    add start files and styling

---
+++ src/components/Footer.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const year = new Date().getFullYear();
+
+function Footer() {
+
+    return (
+    <footer>
+        <p>Copyright &copy; Hanssen Ramanjooloo {year}</p>
+    </footer>
+    );
+}
+
+export default Footer;
---
+++ src/components/Note.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Note() {
+
+    return (
+    <div className="note">
+        <h1>My Note title</h1>
+        <p>My test note here</p>
+    </div>
+    );
+}
+
+export default Note;
