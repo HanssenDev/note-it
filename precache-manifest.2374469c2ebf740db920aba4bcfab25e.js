1d0dc34883ecd100182a0a2f2608c776912721ae
Hanssen <hramanjooloo@gmail.com>
2020-08-20 17:21:21 +0100

    Updates

---
+++ precache-manifest.2374469c2ebf740db920aba4bcfab25e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "34bc3927acc05315eb53ce4d21d59d20",
+    "url": "/note-it/index.html"
+  },
+  {
+    "revision": "defd4cff2f36a7cdefe2",
+    "url": "/note-it/static/js/2.0eca0522.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/note-it/static/js/2.0eca0522.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ce43cb80fc3f798d0711",
+    "url": "/note-it/static/js/main.6e1d8f59.chunk.js"
+  },
+  {
+    "revision": "67792fdeac6ed76dc1c9",
+    "url": "/note-it/static/js/runtime-main.012353a0.js"
+  }
+]);
